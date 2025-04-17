cf448f4afeabdbbe1d16bf6c83b85ca1d0c57c97
Dmytro Ivashchenko <dnytsyk@gmail.com>
2025-04-17 22:54:30 +0200

    rename ToNotifyDTO -> TransactionToNotifyDTO

--- src/main/java/org/example/springbank/dto/TransactionToNotifyDTO.java
+++ src/main/java/org/example/springbank/dto/TransactionToNotifyDTO.java
@@ -2,12 +2,12 @@ package org.example.springbank.dto;
 
 import java.util.Date;
 
-public class ToNotifyDTO {
+public class TransactionToNotifyDTO {
     private final String email;
     private final Date date;
     private final String text;
 
-    public ToNotifyDTO(String email, Date date, String text) {
+    public TransactionToNotifyDTO(String email, Date date, String text) {
         this.email = email;
         this.date = date;
         this.text = text;
