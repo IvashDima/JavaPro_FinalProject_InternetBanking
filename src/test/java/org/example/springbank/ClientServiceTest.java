b75e01b7438f09ef74b804280dfba4210bf74294
Dmytro Ivashchenko <dnytsyk@gmail.com>
2025-04-21 18:49:33 +0200

    optimize ClientServiceTest

--- src/test/java/org/example/springbank/ClientServiceTest.java
+++ src/test/java/org/example/springbank/ClientServiceTest.java
@@ -1,9 +1,19 @@
 package org.example.springbank;
 
+import org.example.springbank.enums.UserRegisterType;
+import org.example.springbank.enums.UserRole;
 import org.example.springbank.models.Client;
+import org.example.springbank.models.CustomUser;
 import org.example.springbank.repositories.ClientRepository;
+import org.example.springbank.repositories.UserRepository;
 import org.example.springbank.services.ClientService;
+import org.example.springbank.services.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Arrays;
 import java.util.List;
@@ -14,27 +24,36 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class ClientServiceTest {
+
+    @Mock
+    private ClientRepository clientMockRepository;
+
+    @InjectMocks
+    private ClientService clientMockService;
+
+    private Client client1, client2;
+
+    @BeforeEach
+    void setUp() {
+        client1 = new Client("Dima", "Berg", "1234567", "dima@test.com", "addressFamily");
+        client2 = new Client("Iryna", "Berg", "1234568", "iryna@test.com", "addressFamily");
+    }
+
     @Test
     public void testGetAllClients() {
-        ClientRepository mockRepo = mock(ClientRepository.class);
-        when(mockRepo.findAll()).thenReturn(Arrays.asList(
-                new Client("Dima", "Berg", "1234567", "dima@test.com", "addressFamily"),
-                new Client("Iryna", "Berg", "1234568", "iryna@test.com", "addressFamily")));
-        ClientService clientService = new ClientService(mockRepo);
+        when(clientMockRepository.findAll()).thenReturn(Arrays.asList(client1, client2));
 
-        List<Client> clients = clientService.getAllClients();
+        List<Client> clients = clientMockService.getAllClients();
         assertEquals(2, clients.size());
     }
 
     @Test
     public void testGetClientByName() {
-        ClientRepository mockRepo = mock(ClientRepository.class);
-        when(mockRepo.findByName("Dima")).thenReturn(Optional.of(
-                new Client("Dima", "Berg", "1234567", "dima@test.com", "addressFamily")));
-        ClientService clientService = new ClientService(mockRepo);
+        when(clientMockRepository.findByName("Dima")).thenReturn(Optional.of(client1));
 
-        Client client = clientService.getByName("Dima");
+        Client client = clientMockService.getByName("Dima");
         assertNotNull(client);
         assertEquals("dima@test.com", client.getEmail());
     }
