d12f1b80ee1368fa3cefea0b5e0ed268dbe8050d
Dmytro Ivashchenko <dnytsyk@gmail.com>
2025-04-25 22:51:20 +0200

    optimize UserController methods

--- src/main/java/org/example/springbank/controllers/UserController.java
+++ src/main/java/org/example/springbank/controllers/UserController.java
@@ -19,7 +19,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.security.Principal;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
