d8a2c84d3bd895214035ee35c20a71a42288ab5d
Dmytro Ivashchenko <dnytsyk@gmail.com>
2025-04-26 16:49:02 +0200

    add LoanService

--- src/main/java/org/example/springbank/exceptions/LoanNotFoundException.java
+++ src/main/java/org/example/springbank/exceptions/LoanNotFoundException.java
@@ -4,4 +4,8 @@ public class LoanNotFoundException extends RuntimeException {
     public LoanNotFoundException(Long id) {
         super("Loan with ID " + id + " not found!");
     }
+
+    public LoanNotFoundException(String message) {
+        super(message);
+    }
 }
