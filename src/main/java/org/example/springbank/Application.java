839adc0d7e31023834a66225fc2e2a6a8a10ff0b
Dmytro Ivashchenko <dnytsyk@gmail.com>
2025-04-28 16:17:42 +0200

    add logging, update application-prod.properties & *.jar

--- src/main/java/org/example/springbank/Application.java
+++ src/main/java/org/example/springbank/Application.java
@@ -2,11 +2,15 @@ package org.example.springbank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class Application {
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
+        logger.info("Starting the application...");
         SpringApplication.run(Application.class, args);
     }
 
